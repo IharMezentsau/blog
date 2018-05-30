9fdaaf9fa7cf589a5c77a471cb2414b6a4eabcf4
Ihar Mezentsau <iharmezentsau@gmail.com>
2018-05-30 20:07:13 +0300

    form chat

--- index.php
+++ index.php
@@ -40,6 +40,40 @@
         </div>
     </div>
 
+
+    <div class="container masonry" data-columns>
+        <div class="item">
+            <div class="thumbnail">
+                <div class="caption">
+                    <p>Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren </p>
+                    <div class="container">
+                        <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10"></div>
+                        <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
+                            <h5>
+                                Автор
+                            </h5>
+                        </div>
+                        <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
+                            <h5>
+                                Дата
+                            </h5>
+                        </div>
+                    </div>
+                    <a href="#spoiler-1" data-toggle="collapse" class="btn btn-primary collapsed spoiler">Ответ</a>
+                    <div class="collapse" id="spoiler-1">
+                        <div class="well">
+                            <p>Параграф с текстом</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="js/bootstrap.js"></script>
     <script>
