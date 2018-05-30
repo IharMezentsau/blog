ca018e1b6ccd56bf567f67574c2ee10bab05387f
Ihar Mezentsau <iharmezentsau@gmail.com>
2018-05-30 19:04:24 +0300

    create navbar

--- index.php
+++ index.php
@@ -10,9 +10,42 @@
 
 </head>
 <body>
-    
+
+    <div class="navbar navbar-inverse navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+
+                <a class="navbar-brand" href="#">Б</a>
+            </div>
+
+            <form action="" class="navbar-form navbar-right">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="E-mail" value="">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" placeholder="Пароль" value="">
+                </div>
+                <button type="submit" class="btn btn-primary">
+                    <i class="fas fa-sign-in-alt"></i> ВОЙТИ
+                </button>
+
+                <a href="#" class="btn btn-info" data-toggle="tooltip"
+                   data-placement="bottom" title="Регистрация">
+                    <i class="far fa-address-card"></i>
+                </a>
+            </form>
+
+            </div>
+
+        </div>
+    </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="js/bootstrap.js"></script>
+    <script>
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    </script>
 </body>
 </html>
