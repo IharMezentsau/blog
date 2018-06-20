9e0df662dbcbb934096d0bf90409a6c256090553
Ihar Mezentsau <iharmezentsau@gmail.com>
2018-06-20 15:24:46 +0300

    add array for message

--- SQL_BD.sql
+++ SQL_BD.sql
@@ -3,7 +3,7 @@ CREATE SCHEMA blog_user;
 
 DROP TABLE IF EXISTS blog_user.t_user;
 CREATE TABLE blog_user.t_user(
-	`id` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
+	`id` INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT UNIQUE,
     `email` VARCHAR(255) NOT NULL UNIQUE,
     `password` VARCHAR(255) NOT NULL UNIQUE,
     `name` VARCHAR(255),
@@ -13,4 +13,30 @@ CREATE TABLE blog_user.t_user(
     
 );
 
+DROP TABLE IF EXISTS blog_user.t_message;
+CREATE TABLE blog_user.t_message (
+	`id` INTEGER NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT,
+    `date` DATETIME NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `message` TEXT NOT NULL,
+    
+    FOREIGN KEY (user_id) REFERENCES blog_user.t_user(id) ON UPDATE CASCADE ON DELETE CASCADE
+    
+);
+
+DROP TABLE IF EXISTS blog_user.t_answer_message;
+CREATE TABLE blog_user.t_answer_message (
+	`id` INTEGER NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT,
+    `date` DATETIME NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `message_id` INTEGER NOT NULL,
+    `answer` TEXT NOT NULL,
+    
+    FOREIGN KEY (user_id) REFERENCES blog_user.t_user(id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY (message_id) REFERENCES blog_user.t_message(id) ON UPDATE CASCADE ON DELETE CASCADE
+    
+);
+
+SELECT * FROM blog_user.t_user;
+UPDATE blog_user.t_user SET validreg = 1 WHERE id = TRUE;
 SELECT * FROM blog_user.t_user;
--- blog.php
+++ blog.php
@@ -1,30 +1,92 @@
-<div class="container masonry" data-columns>
-    <div class="item">
-        <div class="thumbnail">
-            <div class="caption">
-                <p>Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren </p>
-                <div class="container">
-                    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                        <h5>
-                            Автор
-                        </h5>
-                    </div>
-                    <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10">
+<?php
 
-                    </div>
-                    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                        <h5>
-                            Дата
-                        </h5>
-                    </div>
-                </div>
-                <a href="#spoiler-1" data-toggle="collapse" class="btn btn-primary collapsed spoiler">Ответ</a>
-                <div class="collapse" id="spoiler-1">
-                    <div class="well">
-                        <p>Параграф с текстом</p>
+    $resultMessage = mysqli_query($link, 'SELECT * FROM blog_user.t_message INNER JOIN 
+                                                blog_user.t_user ON blog_user.t_message.user_id = blog_user.t_user.id;');
+
+    $resultAnswer = mysqli_query($link, 'SELECT * FROM blog_user.t_answer_message INNER JOIN 
+                                                blog_user.t_user ON blog_user.t_answer_message.user_id = blog_user.t_user.id
+                                                INNER JOIN blog_user.t_message ON 
+                                                blog_user.t_answer_message.message_id = blog_user.t_message.id;');
+
+    while ($row = mysqli_fetch_assoc($resultMessage)){
+    echo '<div class="container masonry" data-columns>
+            <div class="item">
+                <div class="thumbnail">
+                    <div class="caption">
+                        <p>' . $row['blog_user.t_message.message'] . '</p>' .
+                        '<div class="container">
+                            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
+                                <h5>' . $row['blog_user.t_user.name'] . '</h5>' .
+                            '</div>
+                            <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10">
+
+                            </div>
+                            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
+                                <h5>' . $row['blog_user.t_message.date'] . '</h5>' .
+                            '</div>
+                        </div>
+                        <a href="#spoiler-1" data-toggle="collapse" class="btn btn-primary collapsed spoiler">Ответ</a>
+                        <div class="collapse" id="spoiler-1">
+                            <div class="well">';
+        while ($row = mysqli_fetch_assoc($resultAnswer)){
+                        echo '<div class="container masonry" data-columns>
+                                <div class="item">
+                                    <div class="thumbnail">
+                                        <div class="caption">
+                                            <p>' . $row['blog_user.t_answer_message.answer'] . '</p>' .
+                                            '<div class="container">
+                                                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
+                                                    <h5>' . $row['blog_user.t_user.name'] . '</h5>' .
+                                                '</div>
+                                                <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10">
+                    
+                                                </div>
+                                                <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
+                                                    <h5>' . $row['blog_user.t_answer_message.date'] . '</h5>' .
+                                                '</div>
+                                            </div>
+                                    
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>';};
+    echo '
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-</div>
+        </div>';};
+
+
+//<div class="container masonry" data-columns>
+   // <div class="item">
+      //  <div class="thumbnail">
+        //    <div class="caption">
+                //<p>Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren </p>
+                //<div class="container">
+                    //<div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
+                        //<h5>
+                        //    Автор
+                      //  </h5>
+                    //</div>
+                   // <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10">
+
+                   // </div>
+                 //   <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
+                        //<h5>
+                      //      Дата
+                    //    </h5>
+                  //  </div>
+                //</div>
+                //<a href="#spoiler-1" data-toggle="collapse" class="btn btn-primary collapsed spoiler">Ответ</a>
+                //<div class="collapse" id="spoiler-1">
+              //      <div class="well">
+            //            <p>Параграф с текстом</p>
+          //          </div>
+        //        </div>
+      //      </div>
+    //    </div>
+  //  </div>
+//</div>
+            ?>
