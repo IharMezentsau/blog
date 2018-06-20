2b5fa83836757c1310ae8ecba01312fd473dc175
Ihar Mezentsau <iharmezentsau@gmail.com>
2018-06-20 22:55:20 +0300

    add answer form

--- blog.php
+++ blog.php
@@ -8,9 +8,9 @@
         $addMessage = mysqli_query($link, $querySendMessage);
     };
 
-    if (isset($_REQUEST['newAnswer']) && ($_REQUEST['newAnswer'] != "")){
+    if (isset($_REQUEST['newAnswer']) && ($_REQUEST['newAnswer'] != "") && (isset($_REQUEST['message_id'] ))){
         $querySendAnswer = 'INSERT INTO t_answer_message(`date`, `user_id`, `message_id`, `answer`) VALUES ("' . $dateMessage .
-            '",' . $_SESSION['user_id'] . ',' . $_SESSION['message_id'] . ',"' . $_REQUEST['newAnswer'] . '");';
+            '",' . $_SESSION['user_id'] . ',' . $_REQUEST['message_id'] . ',"' . $_REQUEST['newAnswer'] . '");';
         $addAnswer = mysqli_query($link, $querySendAnswer);
     };
 
@@ -52,82 +52,65 @@
                             <a href="#spoiler-' . $row['id_message'] . '" data-toggle="collapse" class="btn btn-primary collapsed spoiler">Ответ</a>
                             <div class="collapse" id="spoiler-' . $row['id_message'] . '">
                                 <div class="well">';
-            while ($row = mysqli_fetch_assoc($resultAnswer)){
-                            echo '<div class="container masonry" data-columns>
-                                    <div class="item">
-                                        <div class="thumbnail">
-                                            <div class="caption">
-                                                <p>' . $row['answer'] . '</p>' .
-                                                '<div class="container">
-                                                    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                                                        <h5>' . $row['user_name_answer'] . '</h5>' .
-                                                    '</div>
-                                                    <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10">
+
+        $message_id = $row['id_message'];
+
+        while ($row = mysqli_fetch_assoc($resultAnswer)){
+            echo                    '<div class="container masonry" data-columns>
+                                        <div class="item">
+                                            <div class="thumbnail">
+                                                <div class="caption">
+                                                    <p>' . $row['answer'] . '</p>' .
+                                                        '<div class="container">
+                                                            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
+                                                                <h5>' . $row['user_name_answer'] . '</h5>' .
+                                                            '</div>
+                                                            <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10">
                         
-                                                    </div>
-                                                    <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                                                        <h5>' . $row['date_answer'] . '</h5>' .
-                                                    '</div>
+                                                            </div>
+                                                            <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
+                                                                <h5>' . $row['date_answer'] . '</h5>' .
+                                                            '</div>
+                                                        </div>  
                                                 </div>
-                                        
                                             </div>
                                         </div>
-                                    </div>
-                                </div>';};
-        echo '
+                                    </div>';};
+        if (isset($_SESSION['user_id'])) {
+            echo '                              
+                                    <div class="container">
+                                        <form action="index.php" name="sendAnswer" class="navbar-form navbar-center" method="post">
+                                           <div class="form-group">
+                                               <input type="text" class="form-control" name="newAnswer" value="">                                                                             
+                                           </div>
+                                           <button type="submit"  class="btn btn-primary" name="message_id" value="' . $message_id . '">
+                                               <i class="fas fa-share-square"></i>
+                                           </button>    
+                                        </form>
+                                    </div>';
+
+            };
+        echo '                  
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>';
+    };
+
+    if (isset($_SESSION['user_id'])) {
+       echo '
+            <div class="navbar navbar-inverse navbar-static-bottom">
+                <div class="container">
+                    <form action="index.php" name="sendMessage" class="navbar-form navbar-center" method="post">
+                        <div class="form-group">
+                            <input type="text" class="form-control" name="newMessage" value="">
+                        </div>
+                        <button type="submit" class="btn btn-primary">
+                            <i class="fas fa-share-square"></i>
+                        </button>    
+                    </form>
+                </div>
             </div>';};
-
-
-//<div class="container masonry" data-columns>
-   // <div class="item">
-      //  <div class="thumbnail">
-        //    <div class="caption">
-                //<p>Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren Loren </p>
-                //<div class="container">
-                    //<div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                        //<h5>
-                        //    Автор
-                      //  </h5>
-                    //</div>
-                   // <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10">
-
-                   // </div>
-                 //   <div class="col-lg-1 col-md-1 col-sm-1 col-xs-1">
-                        //<h5>
-                      //      Дата
-                    //    </h5>
-                  //  </div>
-                //</div>
-                //<a href="#spoiler-1" data-toggle="collapse" class="btn btn-primary collapsed spoiler">Ответ</a>
-                //<div class="collapse" id="spoiler-1">
-              //      <div class="well">
-            //            <p>Параграф с текстом</p>
-          //          </div>
-        //        </div>
-      //      </div>
-    //    </div>
-  //  </div>
-//</div>
-?>
-<?php
-
-if (isset($_SESSION['user_id'])) {
-   echo '
-        <div class="navbar navbar-inverse navbar-static-bottom">
-            <div class="container">
-                <form action="index.php" name="sendMessage" class="navbar-form navbar-center" method="post">
-                    <div class="form-group">
-                        <input type="text" class="form-control" name="newMessage" value="">
-                    </div>
-                    <button type="submit" class="btn btn-primary">
-                        <i class="fas fa-share-square"></i>
-                    </button>    
-                </form>
-            </div>
-        </div>';};
 ?>
